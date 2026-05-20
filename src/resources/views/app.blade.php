0ea1e6578a0893b6750ce31ccd06c54d0f5622ed
starscy <starscy@rambler.ru>
2026-05-20 22:46:59 +0300

    delete fonts

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -6,10 +6,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
     <!-- Scripts & Styles -->
     @routes
     @vite(['resources/css/app.css', 'resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
