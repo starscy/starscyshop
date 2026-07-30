32448bfc362fab39ccb1b670c0fa3903445825e0
starscy <starscy@rambler.ru>
2026-07-30 17:11:50 +0300

    fix

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Models\User;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
